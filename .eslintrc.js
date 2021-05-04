1a6a06507f5c76c192c99dc7e38fffd7d9dc5182
Divyanshu <divyanshu.saini@ndsglobal.com>
2021-05-04 15:28:02 +0530

    updated linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
 
     // https://github.com/prettier/eslint-config-prettier#installation
     // usage with Prettier, provided by 'eslint-config-prettier'.
-    'plugin:prettier/recommended',
+    // 'plugin:prettier/recommended',
     'prettier'
   ],
 
