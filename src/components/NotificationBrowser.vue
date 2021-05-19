f9465441286681b93938471500d4e44b65cbfe68
Divyanshu <divyanshu.saini@ndsglobal.com>
2021-05-19 15:09:12 +0530

    added unauth role ans styles to notification

--- src/components/NotificationBrowser.vue
+++ src/components/NotificationBrowser.vue
@@ -82,3 +82,12 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.q-table--dense .q-table thead tr, .q-table--dense .q-table tbody tr, .q-table--dense .q-table tbody td{
+  height: 50px;
+}
+.q-table__title{
+  color:rgb(29, 29, 29);
+}
+</style>
