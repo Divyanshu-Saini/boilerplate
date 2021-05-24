84c4ef4f19924c6f82824e87620d506ebad0b07d
Divyanshu <divyanshu.saini@ndsglobal.com>
2021-05-24 13:13:49 +0530

    updated build script

--- desktopbuild/msi.js
+++ desktopbuild/msi.js
@@ -6,25 +6,29 @@ const build_info = require('../package.json');
 const msiCreator = new MSICreator({
   appDirectory: path.resolve(__dirname, '..', 'dist', 'electron', 'ForBetter-win32-x64'),
   description: 'Cross Platform virtual assistance app',
-  exe: 'ForBetter',
-  name: 'ForBetter',
-  manufacturer: 'NDS Global Cloud Solutions',
+  exe: build_info.productName,
+  name: build_info.productName,
+  manufacturer: build_info.author,
   version: build_info.version,
-  outputDirectory: path.resolve(__dirname, '..', 'build', 'package'),
+  outputDirectory: path.resolve(__dirname, '..', 'desktopbuild', 'package'),
   appIconPath: path.resolve(__dirname, '..', 'public', 'favicon.ico')
 });
 
 async function build() {
   // Step 2: Create a .wxs template file
   try {
+    console.log(path.resolve(__dirname, '..', 'desktopbuild', 'package'));
     await msiCreator.create();
+    console.log('Created');
   } catch (err) {
     console.error(err);
   }
 
   // Step 3: Compile the template to a .msi file
   try {
+    console.log('compile start');
     await msiCreator.compile();
+    console.log('done');
   } catch (err) {
     console.error(err);
   }
